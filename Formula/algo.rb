d2208d39c757d32eea2a9b9d7dc2984234869fbc
Won's Life <75774713+akrap147@users.noreply.github.com>
2025-11-21 13:24:07 +0900

    fix: tar.gz to app.jar

--- Formula/algo.rb
+++ Formula/algo.rb
@@ -1,7 +1,7 @@
 class Algo < Formula
   desc "백준 알고리즘 문제 자동 복습 스케줄링 CLI"
   homepage "https://github.com/akrap147/algo-review-cli"
-  url "https://github.com/akrap147/algo-review-cli/archive/refs/tags/v0.1.0.tar.gz"
+  url "https://github.com/akrap147/algo-review-cli/releases/download/v0.1.0/app.jar"
   sha256 "c6d9d5775cf53775d65d647bf6d084fb8e95c4c0affb157e93c5c1c0b658d851"
   license "MIT"
 
